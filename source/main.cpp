6984894ab374c5245efd1acc32fbd6cdffcd7b4e
parednuw <mail@paulwunder.de>
2022-09-30 17:52:53 +0200

    Setup Xcode-project
    Created source-folder for .cpp and .h files, as well as xcode-folder

---
+++ source/main.cpp
@@ -0,0 +1,13 @@
+/*
+	EmployeeManagementSystem
+
+	Created by Paul Wunder on 30.09.22.
+*/
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
