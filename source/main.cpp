92291acf1c61244a753c34964b5f7998c2057223
parednuw <mail@paulwunder.de>
2022-10-06 21:32:37 +0200

    Added dowhile-loop in main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,19 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	
+	std::cout << "Program started." << std::endl;
+	int exit = 0;
+	
+	do{
+		
+		std::cout << "Entered Dowhile-loop." << std::endl;
+		std::cout << "0: exit, 1: one more loop. ... ";
+		std::cin >> exit;
+		
+	}while(exit == 1);
+	
+	std::cout << "Program will exit now." << std::endl;
+	
 	return 0;
 }
