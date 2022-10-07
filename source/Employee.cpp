42552c230467d59823f458228a072f0f30c6d2a2
parednuw <mail@paulwunder.de>
2022-10-07 18:31:30 +0200

    Added entries for employees as well as getter- and setter-functions.

--- source/Employee.cpp
+++ source/Employee.cpp
@@ -1,8 +1,61 @@
-//
-//  Employee.cpp
-//  EmployeeManagementSystem
-//
-//  Created by Paul Wunder on 07.10.22.
-//
-
 #include "Employee.hpp"
+
+Employee::Employee()
+{
+	
+}
+
+Employee::~Employee()
+{
+	
+}
+
+void Employee::setName(std::string name)
+{
+	
+}
+
+void Employee::setDepartment(std::string department)
+{
+	
+}
+
+void Employee::setJobTitle(std::string jobTitle)
+{
+	
+}
+
+void Employee::setSalary(int salary)
+{
+	
+}
+
+void Employee::setDateOfBirth(int day, int month, int year)
+{
+	
+}
+
+std::string Employee::getName()
+{
+	return name;
+}
+
+std::string Employee::getDepartment()
+{
+	return department;
+}
+
+std::string Employee::getJobTitle()
+{
+	return jobTitle;
+}
+
+int Employee::getSalary()
+{
+	return salary;
+}
+
+void Employee::getDateOfBirth()
+{
+	
+}
--- source/Employee.hpp
+++ source/Employee.hpp
@@ -1,13 +1,37 @@
-//
-//  Employee.hpp
-//  EmployeeManagementSystem
-//
-//  Created by Paul Wunder on 07.10.22.
-//
-
-#ifndef Employee_hpp
-#define Employee_hpp
+#pragma once
 
 #include <stdio.h>
+#include <iostream>
 
-#endif /* Employee_hpp */
+class Employee
+{
+public:
+	
+	Employee();
+	~Employee();
+	
+	void setName(std::string name);
+	void setDepartment (std::string department);
+	void setJobTitle (std::string jobTitle);
+	void setSalary (int salary);
+	void setDateOfBirth (int day, int month, int year);
+	
+	std::string getName();
+	std::string getDepartment();
+	std::string getJobTitle();
+	int getSalary();
+	void getDateOfBirth();
+	
+private:
+	std::string name;
+	std::string department;
+	std::string jobTitle;
+	int salary;
+	
+	enum dateOfBirth : int
+	{
+		day,
+		month,
+		year
+	};
+};
