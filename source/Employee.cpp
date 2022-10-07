1c50fbde7e32acb3435dcfa6a640feb6198bb093
parednuw <mail@paulwunder.de>
2022-10-07 18:30:04 +0200

    Added Employee cpp and hpp files

---
+++ source/Employee.cpp
@@ -0,0 +1,8 @@
+//
+//  Employee.cpp
+//  EmployeeManagementSystem
+//
+//  Created by Paul Wunder on 07.10.22.
+//
+
+#include "Employee.hpp"
---
+++ source/Employee.hpp
@@ -0,0 +1,13 @@
+//
+//  Employee.hpp
+//  EmployeeManagementSystem
+//
+//  Created by Paul Wunder on 07.10.22.
+//
+
+#ifndef Employee_hpp
+#define Employee_hpp
+
+#include <stdio.h>
+
+#endif /* Employee_hpp */
