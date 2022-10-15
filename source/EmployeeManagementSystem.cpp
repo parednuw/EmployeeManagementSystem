48fb1b9779589bb5467062613f092c9fba7f9e46
parednuw <mail@paulwunder.de>
2022-10-15 22:24:21 +0200

    Added call to add an employee in Run() and added some Logging.

--- source/EmployeeManagementSystem.cpp
+++ source/EmployeeManagementSystem.cpp
@@ -16,23 +16,32 @@ void EmployeeManagementSystem::Run()
 	
 	while(loopIsRunning)
 	{
-		bool isLoopRunning;
 		std::cout << "Entered while-loop" << std::endl;
+		bool isLoopRunning;
+		
+		bool input;
+		std::cout << "Do you want to add an employee? ";
+		std::cin >> input;
+		if (input == 1)	//without error-checking
+			addEmployee();
+		
+		std::cin.ignore(INT_MAX, '\n');	//clears all previous input (buffer)
 		std::cout << "Should loop run? ";
 		std::cin >> isLoopRunning;
 		
+		
 		setLoopRunning(isLoopRunning);
-		
-		
-		
 	}
+	
 }
 
 void EmployeeManagementSystem::addEmployee()
 {
 	std::unique_ptr<Employee> newEmployee = std::make_unique<Employee>();
+	std::cout << "In future all input regarding adding an employee gets here." << std::endl;
 	newEmployee->setDateOfBirth(10, 10, 1910);
 	entries.push_back(std::move(newEmployee));
+	std::cout << "Added employee." << std::endl;
 }
 
 void EmployeeManagementSystem::deleteEmployee()
