a80384fc3a15d43abcc90b45e1a5a00b7a6ec94a
parednuw <mail@paulwunder.de>
2022-10-21 11:03:05 +0200

    Refactored Log.hpp a bit.

--- source/Log.hpp
+++ source/Log.hpp
@@ -22,13 +22,13 @@ public:
 	
 	void Log(std::string message, LogType logType, bool logWithTime)
 	{
-		if ( logType == 0)
+		if ( logType == Info)
 		{
 			string = "[INFO] ";
-		}else if ( logType == 1)
+		}else if ( logType == Warning)
 		{
 			string = "[WARNING] ";
-		}else if (logType == 2)
+		}else if (logType == Error)
 		{
 			string = "[ERROR] ";
 		}
@@ -37,12 +37,10 @@ public:
 		{
 			const std::time_t t_c = getTime();
 			std::cout << std::put_time(std::localtime(&t_c), "%F %T ") << string << message << std::endl;
-			//TODO: Add ticks
 		}else if (! logWithTime)
 		{
 			std::cout << string << message << std::endl;
 		}
-		
 	}
 	
 	std::time_t getTime()
